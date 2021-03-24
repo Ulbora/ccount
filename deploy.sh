09f17348c2b095180eb0399ad16aa9ed87cc067f
Ken <kwill1253@gmail.com>
2021-03-23 22:04:24 -0400

    initial

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cp index.html ../../../goworkspace/ccountPwaServer/static/index.html
+cp pkg/ccount_bg.wasm ../../../goworkspace/ccountPwaServer/static/ccount_bg.wasm
+cp pkg/ccount.js ../../../goworkspace/ccountPwaServer/static/ccount.js
+# cp css/style.css ../../../goworkspace/ccountPwaServer/static/css/style.css 
+# cp js/custom.js ../../../goworkspace/static/js/custom.js 
+# cp favicon.ico ../../../goworkspace/static/favicon.icocp 
---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn get_calories_by_day(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    // val.set_text_content(Some("Hello from Rust!"));
+    // val.set_text_content(Some(
+    //     "<div onclick=\"get_calories_by_day('hi ken');\">Test this</div>",
+    // ));
+    val.set_inner_html("<div onclick=\"alert('hi ken');\">Test this</div>");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
